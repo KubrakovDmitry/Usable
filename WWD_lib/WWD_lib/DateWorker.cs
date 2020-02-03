f32379c38a3043bb881a05242a21bb6b14e85588
KubrakovDmitry <51052027+KubrakovDmitry@users.noreply.github.com>
2020-02-03 14:58:11 +0300

    dll for work with Date

---
+++ WWD_lib/WWD_lib/DateWorker.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace WWD_lib
+{
+    public abstract class DateWorker
+    {
+        // вычисление возраста
+        public static int CalculateAge(DateTime birth)
+        {
+            DateTime now = DateTime.Now;        // настоящая дата
+
+            if(DateTime.Compare(birth, now) > 0)
+            {
+                DateTime num = birth;
+                birth = now;
+                now = birth;
+            }
+
+            int age = now.Year - birth.Year;    // возраст
+
+            // поправка по месяцу и/или дню
+            if(now.Month < birth.Month || (now.Month == birth.Month && now.Day < birth.Day))
+            {
+                age -= 1;
+            }
+
+            return age;            
+        }
+
+        // вычисление возраста (перегруженный)
+        public static int CalculateAge(DateTime birth, DateTime now)
+        {
+            if (DateTime.Compare(birth, now) > 0)
+            {
+                DateTime num = birth;
+                birth = now;
+                now = birth;
+            }
+
+            int age = now.Year - birth.Year;    // возраст
+
+            // поправка по месяцу и/или дню
+            if (now.Month < birth.Month || (now.Month == birth.Month && now.Day < birth.Day))
+            {
+                age -= 1;
+            }
+
+            return age;
+        }
+
+        // вычисление количества дней 
+        public static int CaclculateNumberDays(DateTime first, DateTime second)
+        {
+            TimeSpan interval = second - first;
+
+            if(interval.Days < 0)
+            {
+                return Math.Abs(interval.Days);
+            }
+            else
+            {
+                return interval.Days;
+            }
+        }
+    }
+}
